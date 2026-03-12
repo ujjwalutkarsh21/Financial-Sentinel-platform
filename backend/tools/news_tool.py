d9f45c8d5a05ecb1311dac18290301ab8ead3a4d
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-12 11:46:40 +0530

    news tool created

---
+++ backend/tools/news_tool.py
@@ -0,0 +1,14 @@
+from duckduckgo_search import DDGS
+
+
+def search_news(company: str):
+
+    query = f"{company} stock news"
+
+    results = []
+
+    with DDGS() as ddgs:
+        for r in ddgs.text(query, max_results=5):
+            results.append(r["title"])
+
+    return results
