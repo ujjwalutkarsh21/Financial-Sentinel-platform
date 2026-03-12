ade1572a1d6097030c7575b31f41643094bbd8eb
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-12 16:04:04 +0530

    news agent created

---
+++ backend/agents/news_agent.py
@@ -0,0 +1,30 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.tools.duckduckgo import DuckDuckGoTools
+from textwrap import dedent
+
+
+news_agent = Agent(
+    model=Groq(id="openai/gpt-oss-120b"),
+
+    tools=[
+        DuckDuckGoTools(
+            enable_search=True,
+            enable_news=True,
+            fixed_max_results=5
+        )
+    ],
+
+    instructions=dedent("""
+    You are a financial news analyst.
+
+    Find the most recent news affecting a stock.
+
+    Summarize the top headlines and explain
+    whether they are bullish, bearish, or neutral.
+
+    Always call search_news before answering.
+    """),
+
+    markdown=True
+)
