7c590480968a726d9f9176eb5985715b2e8475b0
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-13 09:32:34 +0530

    sentiment agent integration in multi agent workflow (app.py)

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from agents.aggregator_agent import aggregator_agent
 from agents.market_Agent import market_agent
 from agents.news_agent import news_agent
+from agents.sentiment_agent import sentiment_agent
+
+from utils.sentiment_prompt import build_sentiment_prompt
 from utils.query_analyzer import analyze_query
 
 def main():
@@ -25,15 +28,21 @@ def main():
             Use this structured data to answer the question.
             """
 
-        # response = stock_agent.run(structured_prompt)
-        # response = stock_agent.run(query)
+        #RUN AGENT ----------------------------------------
+        
         market_result = market_agent.run(structured_prompt)
         news_result = news_agent.run(structured_prompt)
+
+        #SENTIMENT AGENT ----------------------------------
+
+        sentiment_prompt = build_sentiment_prompt(news_result.content)
+        sentiment_result = sentiment_agent.run(sentiment_prompt)
 
         combined_input = f"""
         user query : {structured_prompt}
         Market Agent output: {market_result.content}
         News Agent output: {news_result.content}
+        Sentiment Analysis: {sentiment_result}
         """
         response = aggregator_agent.run(combined_input)
 
